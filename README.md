6961de6361286a0d0eeaa644a0f9274ca7600dd2
Rajitha Sittampalam <sittampa@sheridancollege.ca>
2020-01-28 20:45:06 -0500

    Add codes and descriptions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Hello World!
 What a cool place!
+helloworld.py ask if you would like to learn python
+if Yes then, it will show website for your reference
+if No, it will print no problem
--- helloworld.py
+++ helloworld.py
@@ -6,3 +6,9 @@ A program to say hello
 
 if __name__ == '__main__':
     print('Hello, Git world!')
+question = input("Would you like to learn Python? [Y/N]")
+if question == "n":
+    print("No problem")
+if question == "y"
+    print("That's amazing! Go to https://caps.sheridancollege.ca/products/python-foundations.aspx")
+    
